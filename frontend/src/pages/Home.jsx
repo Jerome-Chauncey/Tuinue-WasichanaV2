39275d3e300a22c990fdf7c79b37439abce1b7a1
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-07-29 05:19:49 +0300

    render fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import fetchWithAuth from '../../api/fetchWithAuth';
-import Button from '../../components/Button';
-import Card from '../../components/Card';
+import fetchWithAuth from '../api/fetchWithAuth'; 
+import Button from '../components/Button';
+import Card from '../components/Card';
 
 const Home = () => {
   const [charities, setCharities] = useState([]);
