ae14dba820efc4febb993eb92b88c406e65b68cd
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-07-29 05:16:03 +0300

    render fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -16,7 +16,8 @@ def create_app():
     db.init_app(app)
     jwt.init_app(app)
     migrate.init_app(app, db)
-    CORS(app)
+
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:10000", "methods": ["GET", "POST", "OPTIONS"], "allow_headers": ["Content-Type", "Authorization"]}})
 
     from .routes import main
     app.register_blueprint(main)
