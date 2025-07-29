84d0f8e49f1dcf53f6abec8acead6aaed0c7a032
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-07-29 19:38:49 +0300

    render fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://dbadmin:jerome9911@localhost:5432/wasichanaV2')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('SECRET_KEY')
     
